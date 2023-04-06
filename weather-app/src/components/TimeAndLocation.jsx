74183df52ecb33c026500eb09ae18a9dd2c1b333
Saurabh Kishore <gks210199@gmail.com>
2023-04-06 11:59:39 -0400

    Time And Location Component Created

---
+++ weather-app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TimeAndLocation() {
+  return (
+    <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">Thursday, April 6 2023| Local Time:  11:49 PM</p>
+    </div>
+  )
+}
+
+export default TimeAndLocation
