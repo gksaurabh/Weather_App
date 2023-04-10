66e19a743e01eaef1d0f770c056c08255a938d27
Saurabh Kishore <gks210199@gmail.com>
2023-04-10 00:38:29 -0400

    backend API requests complete

--- weather-app/services/weatherService.js
+++
@@ -1,3 +0,0 @@
-const API_KEY = "1fbfe307c311d65d134b4dcb7fdb170f"
-const BASE_URL = "https://api.openweathermap.org/data/2.5"
-
--- weather-app/src/components/TimeAndLocation.jsx
+++ weather-app/src/components/TimeAndLocation.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import {formatToLocalTime} from "../services/weatherService.js";
 
-function TimeAndLocation() {
+function TimeAndLocation({weather: {dt, timezone, name, country}}) {
   return (
     <div>
         <div className="flex items-center justify-center my-6">
             <p className="text-white text-xl font-extralight">
-                Thursday, April 6 2023| Local Time:  11:49 PM
+                {formatToLocalTime(dt,timezone)}
             </p>
         </div>
 
         <div className="flex items-center justify-center my-3">
             <p className="text-white text-3xl font-medium">
-                Ottawa, ON, Canada
+                {name}, {country}
             </p>
         </div>
     </div>
